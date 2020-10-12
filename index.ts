e927148ab8ffa41bce8847f1786d687e00b7b758
thanhnh <thanhnh.hpvn@gmail.com>
2020-10-12 14:34:20 +0700

    test

--- index.ts
+++ index.ts
@@ -32,7 +32,8 @@ const run2 = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     const promises = [];
     for (let i = 0; i < 50; i++) {
-        const link = allLinks[getRandomInt(allLinks.length)];
+        const link = 'https://m.youtube.com/watch?v=Kzexvhxqov8';
+        // const link = allLinks[getRandomInt(allLinks.length)];
         promises.push((callback) => {
             console.log(i, link);
             view(link)
