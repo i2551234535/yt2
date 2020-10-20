e221f1661e93dd373d8377e0b9517deb43611bbf
viettd93 <viet751993@gmail.com>
2020-10-20 16:31:02 +0700

    update view tv

--- utils/view8.ts
+++ utils/view8.ts
@@ -1,5 +1,5 @@
-import { devices, webkit, WebKitBrowser } from 'playwright';
-import { ProfileModel } from '../models/Profile.model';
+import { ViewportSize, webkit, WebKitBrowser } from 'playwright';
+import { ProfileTVModel } from '../models/ProfileTV.model';
 import { delay } from './delay';
 
 function getRandomArbitrary(min: number, max: number) {
@@ -9,6 +9,7 @@ function getRandomArbitrary(min: number, max: number) {
 export const view = async (url: string) => {
     let running = true;
     return new Promise(async (resolve, reject) => {
+        const now = new Date().getTime();
         let browser: WebKitBrowser | undefined;
         let step = 0;
         setTimeout(async () => {
@@ -22,14 +23,42 @@ export const view = async (url: string) => {
         });
         step = 1;
 
+        const totalProfile = await ProfileTVModel.find({}).countDocuments();
+        const profileData = await ProfileTVModel.findOne({}).skip(getRandomArbitrary(0, totalProfile));
+
+        // profileData.is_running = true;
+        // await profileData.save();
+
+        let viewport: ViewportSize = {
+            width: 1280,
+            height: 720,
+        };
+
+        switch (profileData.size_type) {
+            case 1:
+                viewport = {
+                    width: 1366,
+                    height: 768,
+                };
+                break;
+            case 2:
+                viewport = {
+                    width: 1920,
+                    height: 1080,
+                };
+                break;
+            case 3:
+                viewport = {
+                    width: 1360,
+                    height: 760,
+                };
+                break;
+        }
+
         try {
             const context = await browser.newContext({
-                userAgent:
-                    'Mozilla/5.0 (SMART-TV; LINUX; Tizen 5.5) AppleWebKit/537.36 (KHTML, like Gecko) 69.0.3497.106.1/5.5 TV Safari/537.36',
-                viewport: {
-                    width: 1280,
-                    height: 720,
-                },
+                userAgent: profileData.user_agent,
+                viewport,
             });
 
             const page = await context.newPage();
@@ -38,13 +67,30 @@ export const view = async (url: string) => {
             const timeout = getRandomArbitrary(40000, 80000);
             console.log(timeout);
 
-            await page.goto('https://www.youtube.com/tv#/watch?v=c0l3Km1IQfE');
+            await page.goto(url);
+            await delay(3000);
+            await page.press('body', 'Enter');
+            await delay(10000);
+            await page.press('body', 'Enter');
 
             await delay(timeout);
             console.log(await context.cookies());
+            await ProfileTVModel.findOneAndUpdate(
+                {
+                    _id: profileData._id,
+                },
+                {
+                    $set: {
+                        cookies: JSON.stringify(await context.cookies()),
+                        last_time: now,
+                    },
+                },
+            );
         } catch (error) {
             throw error;
         } finally {
+            profileData.last_time = now;
+            await profileData.save();
             await browser.close();
             resolve();
         }
