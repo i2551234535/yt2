dc11fefae68b83d7f3611da60c2c8d09672ddf50
viettd93 <viet751993@gmail.com>
2020-10-30 17:35:58 +0700

    remove log http

--- utils/view14.ts
+++ utils/view14.ts
@@ -60,8 +60,8 @@ const proxyHTTP = (route: Route, url, method, headers, data) => {
         },
     })
         .then((data) => {
-            console.log(method, url);
-            console.log(data.status);
+            // console.log(method, url);
+            // console.log(data.status);
             return route.fulfill({
                 status: data.status,
                 body: data.data,
