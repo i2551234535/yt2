7cb457757ba4b645eb3c9d39d970a8186e7f6f0a
viettd93 <viet751993@gmail.com>
2020-10-30 13:50:37 +0700

    remove log

--- utils/view12.ts
+++ utils/view12.ts
@@ -35,8 +35,8 @@ const proxy = (route: Route, url, method, headers, data) => {
         },
     })
         .then((data) => {
-            console.log(method, url);
-            console.log(data.status);
+            // console.log(method, url);
+            // console.log(data.status);
             return route.fulfill({
                 status: data.status,
                 body: data.data,
