565fc8c61e81a1fa3c293543b45d9160c3763f90
viettd93 <viet751993@gmail.com>
2020-11-14 11:04:18 +0700

    Revert "update"
    This reverts commit 2127b906427bbc1e5dc61b4e75d965f0059c0172.

--- utils/view16.ts
+++ utils/view16.ts
@@ -196,19 +196,16 @@ export const view = async (url: string) => {
                 return route.continue();
             });
 
-            const random = getRandomArbitrary(0, 7);
-            let timeout = getRandomArbitrary(2 * 60 * 1000, 3 * 60 * 1000);
+            const random = getRandomArbitrary(0, 60);
+            let timeout = getRandomArbitrary(1.5 * 60 * 1000, 2 * 60 * 1000);
             console.log('random:', random);
             if (isViewed) {
-                await viewDirect(page, url);
-                // await viewRandomAtYoutube(page);
+                await viewRandomAtYoutube(page);
             } else if (random === 0) {
-                await viewDirect(page, url);
-                // await page.goto('https://m.youtube.com');
-                // timeout = 10000;
+                await page.goto('https://m.youtube.com');
+                timeout = 10000;
             } else if (random === 1) {
-                await viewDirect(page, url);
-                // await viewRandomAtYoutube(page);
+                await viewRandomAtYoutube(page);
                 // viewUrl = true;
                 // await viewAtYoutubeSuggest(page, url);
             } else if (random === 2) {
